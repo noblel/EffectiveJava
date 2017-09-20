4271a3e96554b21305bbea21738f54cc55f4f06d
noblel <357384397@qq.com>
2017-09-20 15:26:40 +0800

    item15

---
+++ src/chapter04/item15/Complex.java
@@ -0,0 +1,93 @@
+package chapter04.item15;
+
+/**
+ * 1.不要提供任何会修改状态的方法
+ * 2.保证类不会被拓展
+ * 3.所有的域都是final的
+ * 4.所有的域都成为私有的
+ * 5.确保任何可变组件的互斥访问
+ * <p>
+ * 不可变对象本质上线程安全的,它们不要求同步
+ *
+ * 缺点:不同的值都需要一个单独的对象
+ *
+ * @author Noblel
+ */
+public final class Complex {
+
+    public static final Complex ZERO = new Complex(0, 0);
+    public static final Complex ONE = new Complex(1, 0);
+    public static final Complex I = new Complex(0, 1);
+
+    private final double re;
+    private final double im;
+
+    private Complex(double re, double im) {
+        this.re = re;
+        this.im = im;
+    }
+
+    public static Complex valueOf(double re, double im) {
+        return new Complex(re, im);
+    }
+
+    public static Complex valueOfPolar(double r, double theta) {
+        return new Complex(r * Math.cos(theta), r * Math.sin(theta));
+    }
+
+    public double realPart() {
+        return re;
+    }
+
+    public double imaginaryPart() {
+        return im;
+    }
+
+    //返回新实例,而不是修改
+    public Complex add(Complex c) {
+        return new Complex(re + c.re, im + c.im);
+    }
+
+    public Complex subtract(Complex c) {
+        return new Complex(re - c.re, im - c.im);
+    }
+
+    public Complex multiply(Complex c) {
+        return new Complex(re * c.re - im * c.im, re * c.im + im * c.re);
+    }
+
+    public Complex divide(Complex c) {
+        double tmp = c.re * c.re + c.im * c.im;
+        return new Complex((re * c.re + im * c.im) / tmp, (im * c.re - re
+                * c.im)
+                / tmp);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof Complex))
+            return false;
+        Complex c = (Complex) o;
+
+        return Double.compare(re, c.re) == 0 && Double.compare(im, c.im) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 17 + hashDouble(re);
+        result = 31 * result + hashDouble(im);
+        return result;
+    }
+
+    private int hashDouble(double val) {
+        long longBits = Double.doubleToLongBits(val);
+        return (int) (longBits ^ (longBits >>> 32));
+    }
+
+    @Override
+    public String toString() {
+        return "(" + re + " + " + im + "i)";
+    }
+}
