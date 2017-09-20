8dc0b5c07da8dc8d84fee590e27d53354ff6b676
noblel <357384397@qq.com>
2017-09-20 15:26:29 +0800

    item14

---
+++ src/chapter04/item14/Time.java
@@ -0,0 +1,23 @@
+package chapter04.item14;
+
+/**
+ * 公有类永远都不一个个暴露可变的域
+ *
+ * @author Noblel
+ */
+public final class Time {
+    private static final int HOURS_PER_DAY = 24;
+    private static final int MINUTES_PER_HOUR = 60;
+
+    public final int hour;
+    public final int minute;
+
+    public Time(int hour, int minute) {
+        if (hour < 0 || hour >= HOURS_PER_DAY)
+            throw new IllegalArgumentException("Hour: " + hour);
+        if (minute < 0 || minute >= MINUTES_PER_HOUR)
+            throw new IllegalArgumentException("Min: " + minute);
+        this.hour = hour;
+        this.minute = minute;
+    }
+}
